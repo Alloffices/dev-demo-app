d0048553f9e8a67f7a656387d4c7bf922d3a0957
ao <alloffices.nyc@gmail.com>
2017-11-04 17:26:12 -0400

    Not authorized projects index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,19 +2,30 @@ require 'will_paginate/array'
 
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
   before_action :authorize_admin
 
   # GET /projects
   # GET /projects.json
   def index
-    @projects = current_user.projects.all.sort_by {|i| i.start_date - i.end_date}.reverse!.paginate(:page => params[:page], :per_page => 10)
-    @projects = current_user.projects.all.sort_by {|i| i.start_date - i.end_date}.reverse!.paginate(:page => params[:page], :per_page => 10)
+    @projects = Project.all.order("created_at desc")
+    @projects = Project.all.sort_by {|i| i.start_date - i.end_date}.reverse!.paginate(:page => params[:page], :per_page => 10)
+    @projects = Project.all.sort_by {|i| i.start_date - i.end_date}.reverse!.paginate(:page => params[:page], :per_page => 10)
     @your_int = []
-    current_user.projects.sort_by {|i| i.start_date - i.end_date}.reverse!.each_with_index do |project, index|
+    Project.all.sort_by {|i| i.start_date - i.end_date}.reverse!.each_with_index do |project, index|
       @your_int << {id: index, start_date: project.start_date, end_date: project.end_date}
     end
     gon.your_int = @your_int
+
+    if user_signed_in? 
+      @projects = Project.all.sort_by {|i| i.start_date - i.end_date}.reverse!.paginate(:page => params[:page], :per_page => 10)
+      @projects = Project.all.sort_by {|i| i.start_date - i.end_date}.reverse!.paginate(:page => params[:page], :per_page => 10)
+      @your_int = []
+      current_user.projects.sort_by {|i| i.start_date - i.end_date}.reverse!.each_with_index do |project, index|
+        @your_int << {id: index, start_date: project.start_date, end_date: project.end_date}
+      end
+      gon.your_int = @your_int
+    end
   end
 
   # GET /projects/1
@@ -85,7 +96,6 @@ class ProjectsController < ApplicationController
     end
 
     def authorize_admin
-      redirect_to root_path, alert: "Permissions denied" unless
-      current_user.admin?
+      :authenticate_user! && current_user.try(:admin?)
     end
 end
